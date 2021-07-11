5abf18e5fe67e5c95ff9a6b245e7f332ed2eb805
Yoga Pranata <ahmadzaylani001@gmail.com>
2021-07-11 19:45:49 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -4,7 +4,9 @@ I'm URL Uploader!
 You can upload HTTP/HTTPS direct link, Using this bot!
 
 /help for more details!
-Made with ❤ by @Yoga_CIC"""
+
+Made with ❤ by @Yoga_CIC
+Thanks to @xgorn, @RxyMX"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
